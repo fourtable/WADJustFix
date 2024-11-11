fa8ffd2365d54f9d1cb2eb7fa97b7b27ba16e042
fourtable <peien.seah.2023@smu.edu.sg>
2024-11-11 14:38:49 +0800

    added server timestamp

--- JustFix/src/views/ApproveEvent.vue
+++ JustFix/src/views/ApproveEvent.vue
@@ -39,7 +39,7 @@
 import { mapActions, mapGetters } from 'vuex';
 import { db, realtimeDb} from "../main";
 import { collection, getDoc, addDoc, doc, updateDoc, arrayUnion } from "firebase/firestore";
-import { ref as dbRef, get, query as dbquery, set, onValue, update, push, remove } from 'firebase/database';
+import { ref as dbRef, get, query as dbquery, set, onValue, update, push, remove, serverTimestamp } from 'firebase/database';
 
 export default {
   computed: {
